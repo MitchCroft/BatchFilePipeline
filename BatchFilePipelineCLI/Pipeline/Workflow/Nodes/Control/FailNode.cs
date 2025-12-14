41a8c25fcc1343cc8855711fa8cf0a4f432ab2c8
Mitch Croft <mitchell.croft@email.com>
2025-12-14 17:17:50 +1100

    add: Add Fail Node
    Added a node that can be used to intentionally cause a failure within the processing graph

---
+++ BatchFilePipelineCLI/Pipeline/Workflow/Nodes/Control/FailNode.cs
@@ -0,0 +1,62 @@
+﻿using BatchFilePipelineCLI.PropertyResolver;
+
+namespace BatchFilePipelineCLI.Pipeline.Workflow.Nodes.Control
+{
+    /// <summary>
+    /// Raise an intentional failure in the process for the given entry
+    /// </summary>
+    [PipelineNode(nameof(FailNode), NodeUsage.Process)]
+    internal sealed class FailNode : IPipelineNode
+    {
+        /*----------Variables----------*/
+        //PRIVATE
+
+        /// <summary>
+        /// The different properties that can be used when intentionally causing a failure event
+        /// </summary>
+        private readonly Property _codeProperty = Property.Create
+        (
+            "Code",
+            "The result code that will be used for the failure message",
+            -1
+        );
+        private readonly Property _messageProperty = Property.Create
+        (
+            "Message",
+            "The message that should be used as the failure reason",
+            "Fail Node Reached"
+        );
+
+        /*----------Functions----------*/
+        //PUBLIC
+
+        /// <summary>
+        /// Retrieve the collection of input properties that can be defined for processing the Node
+        /// </summary>
+        /// <returns>Retrieve the collection of input properties that can be used by the Node for Processing</returns>
+        public IList<Property> GetInputProperties() => [_codeProperty, _messageProperty];
+
+        /// <summary>
+        /// Retrieve the collection of output properties that will be made available for use in later stages
+        /// </summary>
+        /// <returns>Returns the collection of output properties that can be used in later stages for processing</returns>
+        public IList<Property> GetOutputProperties() => Array.Empty<Property> ();
+
+        /// <summary>
+        /// Process the pipeline node with the specified inputs and generate a result
+        /// </summary>
+        /// <param name="inputs">The collection of inputs that have been described for this node</param>
+        /// <param name="cancellationToken">Cancellation token that can be used to control the lifespan of the operation</param>
+        /// <returns>Returns the output result of the Node describing the operation that was performed</returns>
+        public ValueTask<ExecutionResult> ProcessNodeResultAsync(IReadOnlyDictionary<string, object?> inputs,
+                                                                 CancellationToken cancellationToken)
+        {
+            // Get the property details that can be used for the failure event
+            int code = (int)inputs[_codeProperty.Name]!;
+            string message = (string)inputs[_messageProperty.Name]!;
+
+            // Raise the failure
+            return ValueTask.FromResult(new ExecutionResult(code, message));
+        }
+    }
+}
